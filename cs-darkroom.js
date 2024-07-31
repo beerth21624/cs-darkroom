e7adfba4d749137d5b898007615768307bdbf286
beerth <beerzii.4321@gmail.com>
2024-07-31 12:43:19 +0700

    improve style

--- cs-darkroom.js
+++ cs-darkroom.js
@@ -1,44 +1,61 @@
-import io from 'socket.io-client';
-import readline from 'readline';
+import io from 'socket.io-client'
+import readline from 'readline'
+import chalk from 'chalk'
+import figlet from 'figlet'
 
-
-const socket = io('https://general-chat-node.tu4rl4.easypanel.host/');
+const socket = io('https://general-chat-node.tu4rl4.easypanel.host/')
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+  input: process.stdin,
+  output: process.stdout,
+})
+
+console.clear()
+console.log(chalk.green(figlet.textSync('CS-Darkroom', { font: 'Slant' })))
+
+const prompt = () => {
+  process.stdout.write(chalk.cyan('⚡ '))
+}
 
 socket.on('connect', () => {
-    console.log(`Connected to server`);
-});
+  console.log(chalk.yellow('🔒 Secure connection established'))
+  console.log(chalk.magenta('Enter your hacker alias:'))
+  prompt()
+})
 
 socket.on('message', (message) => {
-    console.log(message);
-});
+  process.stdout.clearLine()
+  process.stdout.cursorTo(0)
+  console.log(
+    chalk.green(`[${new Date().toLocaleTimeString()}] `) + chalk.white(message)
+  )
+  prompt()
+})
 
 socket.on('disconnect', () => {
-    console.log('Disconnected from server');
-    rl.close();
-});
+  console.log(chalk.red('❌ Connection terminated'))
+  rl.close()
+})
 
 socket.on('error', (err) => {
-    console.error(`Connection error: ${err.message}`);
-});
+  console.error(chalk.red(`⚠️ Connection error: ${err.message}`))
+})
 
 rl.on('line', (input) => {
-    if (!socket.nameSet) {
-        socket.emit('setName', input);
-        socket.nameSet = true;
-    } else {
-        socket.emit('chatMessage', input);
-    }
-});
+  if (!socket.nameSet) {
+    socket.emit('setName', input)
+    socket.nameSet = true
+    console.log(chalk.yellow('Identity confirmed. Entering the matrix...'))
+  } else {
+    socket.emit('chatMessage', input)
+  }
+  prompt()
+})
 
 process.on('SIGINT', () => {
-    console.log('\nDisconnecting from server...');
-    socket.disconnect(() => {
-        console.log('Disconnected.');
-        process.exit(0);
-    });
-});
+  console.log(chalk.yellow('\nInitiating disconnect sequence...'))
+  socket.disconnect(() => {
+    console.log(chalk.red('Disconnected. Exiting the matrix.'))
+    process.exit(0)
+  })
+})
